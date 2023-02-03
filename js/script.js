4144002a6108b85c790671e0bf7fb14220d8ef14
MartinaPerozzi <perozzimartina@gmail.com>
2023-02-03 14:52:25 +0100

    html + css + js - structure

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// Preparo gli elementi
+const theYears = document.getElementById("years");
+const theMonths = document.getElementById("month");
+const theDays = document.getElementById("days");
+const theHours = document.getElementById("hours");
+const theMinutes = document.getElementById("minutes");
+const theSeconds = document.getElementById("seconds");
+const titleText = document.querySelector(".mess");
+// Cronometer
+const theDaysLess = document.getElementById("days-crono");
+const theHoursLess = document.getElementById("hours-crono");
+const theMinutesLess = document.getElementById("minutes-crono");
+const theSecondsLess = document.getElementById("seconds-crono");
+
+// // Creare un orologio sfruttando le timing function di JS
+
+timeNow()
+/********************************************
+ *                                          *
+ *                 FUNCTIONS                *
+********************************************/
+
+// Creo una variabile che conterrà l'intervallo della funzione e la ripeterà all'infinito o finchè non la fermo
+const theClock = setInterval(timeNow, 1000);
+
+// Creo una funzione che mi faccia da orologio, mi prende secondi, minuti, ora e giorno corrente.
+function timeNow() {
+    const now = new Date(); //Creo una nuova Date a partire da ora
+
+    const seconds = now.getSeconds() // Prendi i secondi
+    const minutes = now.getMinutes() // Prendi i minuti
+    const hours = now.getHours() //Prendi le ore
+    const days = now.getDay() // Prendi i giorni
+
+    // Scrivili nel posto delegato dell'HTML
+    theSeconds.innerHTML = (seconds < 10) ? "0" + seconds : seconds;
+    theMinutes.innerHTML = (minutes < 10) ? "0" + minutes : minutes;
+    theHours.innerHTML = (hours < 10) ? "0" + hours : hours;
+    theDays.innerHTML = (days < 10) ? "0" + days : days;
+    const cronometerTime = setInterval(passedTime, 1000);
+
+    function passedTime() {
+        let totalSeconds = 100000;
+        ++totalSeconds;
+
+        const seconds = parseInt(totalSeconds % 60);
+        const minutes = parseInt((totalSeconds / 60) % 60);
+        const hours = parseInt((totalSeconds / 60 / 60) % 24);
+        const days = parseInt((totalSeconds / 60 / 60 / 24));
+
+        theSecondsLess.innerHTML = (seconds < 10) ? "0" + seconds : seconds;
+        theMinutesLess.innerHTML = (minutes < 10) ? "0" + minutes : minutes;
+        theHoursLess.innerHTML = (hours < 10) ? "0" + hours : hours;
+        theDaysLess.innerHTML = (days < 10) ? "0" + days : days;
+
+    }
+}
+// Funzione per cronometro
+
+
+
+
+
+
